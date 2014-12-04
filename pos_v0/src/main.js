b7844bbef3a688b9577abed7e91a76c02bde1c19
gaofanfan <1125774809@qq.com>
2014-12-04 13:20:48 +0800

    complete v0

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,19 +1,24 @@
 function printInventory(items) {
   var itemsText = '';
+  var totalamount = 0;
 
   for(var i = 0; i < items.length; i++) {
     var item = items[i];
+    var itemtotal = item.count * item.price;
     itemsText += '名称：' + item.name +
-                 '，数量：' + item.count + item.unit +
-                 '，单价：' + item.price.toFixed(2) +
-                 '(元)，小计：' + (item.count * item.price).toFixed(2) +'(元)\n';
-  }
+      '，数量：' + item.count + item.unit +
+      '，单价：' + item.price.toFixed(2) +
+      '(元)，小计：' + itemtotal.toFixed(2) +'(元)\n';
 
-  var inventorytext = '***<没钱赚商店>购物清单***\n' +
-    itemsText + 
-   '----------------------\n' +
-   '总计：23.00(元)\n' +
-   '**********************';
+    totalamount += itemtotal;
+   }
 
-  console.log(inventorytext);
-}
+   var inventorytext =
+      '***<没钱赚商店>购物清单***\n' +
+      itemsText +
+      '----------------------\n' +
+      '总计：' + totalamount.toFixed(2) + '(元)\n' +
+
+      '**********************';
+   console.log(inventorytext);
+ }
