7de405e4fb5f237a6367f43193635b5f00be3252
gaofanfan <1125774809@qq.com>
2014-12-12 08:24:07 +0800

    Arrangement pos_v2

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -2,6 +2,7 @@ function printInventory(tags) {
 
   var cartItems = CartItem.getCartItems(tags);
   var inventorytext = Inventorytext.getText(cartItems);
+  
   console.log(inventorytext);
 
 }
--- pos_v2/src/model/promotion.js
+++ pos_v2/src/model/promotion.js
@@ -3,12 +3,41 @@ function Promotion(type, barcodes) {
     this.barcodes = barcodes || [];
 }
 
-Promotion.all = function() {
-  return [
-    new Promotion('BUY_TWO_GET_ONE_FREE', [
-    'ITEM000000',
-    'ITEM000001',
-    'ITEM000005'
-    ])
-    ];
-  };
+Promotion.getpromotionText = function(cartItems) {
+  var promotions = loadPromotions();
+  var promotionText = '';
+  _.forEach(cartItems, function(cartItem) {
+    var item = cartItem.item;
+    var promotionitem = _.find(promotions[0].barcodes,function(barcode) {
+      return barcode === item.barcode;
+    });
+
+    if (promotionitem) {
+      promotionText += '名称：' + item.name +
+        '，数量：' + Math.floor(cartItem.count / 3)
+        + item.unit + '\n';
+    }
+  });
+  return promotionText;
+}
+
+  // for(var i = 0; i < cartItems.length; i++) {
+  //   var item = cartItems[i].item;
+  //   var promotionitem = Promotion.findpromotionitem(item.barcode,promotions[0]);
+  //   if( promotionitem) {
+  //     promotionText += '名称：' + item.name +
+  //       '，数量：' + Math.floor(cartItems[i].count / 3)
+  //       + item.unit + '\n';
+  //   }
+  // }
+
+// Promotion.findpromotionitem = function(barcode,promotion) {
+//     var promotionitem;
+//
+//     for(var i = 0;i < promotion.barcodes.length;i++) {
+//       if(promotion.barcodes[i] === barcode) {
+//         promotionitem = promotion.barcodes[i];
+//       }
+//     }
+//     return promotionitem;
+//   }
